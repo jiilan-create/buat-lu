3481e1c12c1da17620c7acaf2ad2bce08e179f07
jiilan-create <66149604+jiilan-create@users.noreply.github.com>
2020-06-06 11:39:23 +0700

    Update buatlu

--- Buatlu .py
+++ Buatlu .py
@@ -4,3 +4,15 @@ def jalan (kata):
        sys.stdout.write(e)
        sys.stdout.flush()
        time.sleep(0.1)
+jalan("Goblok lu beraninya keroyokan dan dibelakang".center(44))
+jalan("nih buat lu bansat".center(44))
+jalan("   -----
+          |   |
+          |   |
+          |   |
+        ---   ---
+    ----|       |----
+  --                --
+|--  Damn you son of --|
+|--  a bitch yes,you --|
+|______________________|".center(44))
